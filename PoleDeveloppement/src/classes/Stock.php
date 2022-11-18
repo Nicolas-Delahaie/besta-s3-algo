c0b82e47e751e274e2e0af752d1828d2c6b07e02
unknown <alexandre.pascal.ep@gmail.com>
2022-11-18 15:49:04 +0100

    V6 classe Stock, modification de la classe stock + ajout des commentaires

--- PoleDeveloppement/src/classes/Stock.php
+++ PoleDeveloppement/src/classes/Stock.php
@@ -1,67 +1,77 @@
 <?php
 
-    include 'boisson.php';
+    /** 
+    *@author @alexandrePascal <apascal003@iutbayonne.univ-pau.fr>
+    *@version 6.0
+    */
 
-    print("tu est dans la classe stock");
+    require_once('boisson.php');
+
+    /**
+     * @brief Classe Stock comportant une liste d'alcools, une liste de diluants et une 
+     */
 
     class Stock{
-
+        
         /* -------------------------------------------------------------------------- */
         /*                                  ATTRIBUTS                                 */
         /* -------------------------------------------------------------------------- */
-        private $lAlcools;
-        private $lDiluants;
-        private $lAutres;
+        private $lAlcools = array(); // Création de la liste d'alcools
+        private $lDiluants = array(); // Création de la liste de diluants
+        private $lAutres = array(); // Création de la liste des autres boissons
 
         /* -------------------------------------------------------------------------- */
         /*                                CONSTRUCTEUR                                */
         /* -------------------------------------------------------------------------- */
         
+        /**
+         * @brief
+         * @param
+         * @warning Si le stock est construit avec une seule boisson, il y aura un problème car le code ne sais pas différentier entre 1 paramètre étant un object boisson, ou un autre étant un objet stock (problème de constructeur par copie). On admet donc que le cas d'une seul boisson n'existera jamais (pour 1 recette il faut au moins 2 boissons)
+         */
         function __construct()
         {
-            $lAlcools = array();
-            $lDiluants = array();
-            $lAutres = array();
+            $nbArguments= func_num_args(); //Utilisation de la d'une fonction récupérant le nombre d'arguments passés en paramètre
+            $tabArguments=func_get_args(); //Utilisation de la fonction permettant de recupérer les arguments passés en paramètre
 
-            $nbArguments= func_num_args();
-            $tabArguments=func_get_args(); 
+            if ($nbArguments == 0) { //Si aucuns argument n'est passé en paramètre
+                $this->lAlcools = []; //Les listes sont initilisées vides
+                $this->lDiluants = [];
+                $this->lAutres = [];
+            }
 
-            if ($nbArguments > 0) {
+            elseif ($nbArguments == 1) //Si un seul argument est passé en paramètre, c'est un constructeur par copie
+            {
+                $this->lAlcools=$tabArguments[0]->lAlcools; //On copie donc chaque attributs de l'objet en paramètre dans la copie
+                $this->lDiluants=$tabArguments[0]->lDiluants;
+                $this->lAutres=$tabArguments[0]->lAutres;
+            }
+
+            else{
                 
-                for ($i = 0; $i <= $nbArguments; $i++) {
+                for ($i = 0; $i <= $nbArguments - 1; $i++) { //Sinon pour chaque argument passé en paramètre :
             
-                    switch ($tabArguments[$i]->getTypeBoisson) {
-                        case 1:
-                            $this->setLAlcools($tabArguments[$i]);
+                    switch ($tabArguments[$i]->getTypeBoisson()) { //On regarde quel est le type de cette boisson
+                        case 1: //Si c'est de type 1 (alccol)
+                            $this->setLAlcools($tabArguments[$i]); //on l'ajoute à la liste d'alcools
                             break;
                         
-                        case 2:
-                            $this->setLDiluants($tabArguments[$i]);  
+                        case 2://Si c'est de type 2 (diluant)
+                            $this->setLDiluants($tabArguments[$i]); //on l'ajoute à la liste de diluants 
                             break;
                         
-                        case 3:
-                            $this->setLAutres($tabArguments[$i]);
+                        case 3: //Si c'est le type 3 (autre)
+                            $this->setLAutres($tabArguments[$i]); //on l'ajoute à la liste de autres boissons
                             break;
-                        default:
+
+                        default: //Si aucun de ces cas ne sont possibles on indique l'erreur possible
                             print("les parametres ne sont pas bons, les paramètres doivent etre soit nuls, soit des objets de type boisson");
                             break;
                     }   
                 }
             }
 
-            else {
-                $this->setLAlcools("");
-                $this->setLDiluants("");
-                $this->setLAutres("");
-            }
-
-            if ($nbArguments > 0) {
-                for ($i = 0; $i <= $nbArguments; $i++) {
-                    array_push($this->lAlcools, $tabArguments[$i]);
-                    
-                }
-            }
-    
+            
         }
 
         /* -------------------------------------------------------------------------- */
@@ -71,40 +81,79 @@
         /* --------------------------------- GETEUR --------------------------------- */
 
         function getLAlcools (){
-            return ($this->lAlcools);
-        }        
+            
+            if ($this->lAlcools != []){ //On regarde si notre liste n'est pas vide
+                $nbAlcools = count($this->lAlcools); //On compte le nombre d'éléments présents dans la liste des alcools
+                $listeDesAlcools = ""; //On prepare le message a afficher
+
+                for ($i = 0; $i <= $nbAlcools - 1; $i++){ //Pour chaque alcool
+                    $nomAlcool= $this->lAlcools[$i]->getNomBoisson(); //On récupére son nom
+                    $listeDesAlcools = $listeDesAlcools." ".$nomAlcool; //On le concatène avec les autres noms
+                    }
+                }
+
+            else { //Si la liste des alcools est vide
+                $listeDesAlcools = "aucuns alcools"; 
+            }    
+            return ($listeDesAlcools);
+        }    
+
         function getLDiluants (){
-            return ($this->lDiluants);
+
+            if ($this->lDiluants != []){ //On regarde si notre liste n'est pas vide
+                $nbDiluant = count($this->lDiluants); //On compte le nombre d'éléments présents dans la liste des diluants
+                $listeDesDiluants = "";//On prepare le message a afficher
+
+                for ($i = 0; $i <= $nbDiluant - 1; $i++){ //pour chaque diluants
+                    $nomDiluant= $this->lDiluants[$i]->getNomBoisson();//On récupère son nom
+                    $listeDesDiluants = $listeDesDiluants." ".$nomDiluant; //On le concatène avec les autres noms
+                    }
+                }
+            else {
+                $listeDesDiluants = "aucuns diluants";
+            }    
+            return ($listeDesDiluants);
         }        
         function getLAutres (){
-            return ($this->lAutres);
+
+            if ($this ->lAutres != []) {//On regarde si notre liste n'est pas vide
+                $nbAutre = count($this->lAutres); //On compte le nombre d'éléments présents dans la liste des autres boissons
+                $listeDesAutres = "";//On prepare le message a afficher
+                
+                for ($i = 0; $i <= $nbAutre - 1; $i++){ //pour chaque autres boissons
+                    $nomAutre= $this->lAutres[$i]->getNomBoisson(); //on récupère son nom
+                    $listeDesAutres = $listeDesAutres." ".$nomAutre; //On le concatène avec les autres noms
+                    }
+            }
+
+            else {
+                $listeDesAutres = "aucunes autres boissons";
+            }
+            return ($listeDesAutres);
         }              
 
         /* --------------------------------- SETEUR --------------------------------- */
 
-        function setLAlcools($nouveauAlcool){
-                array_push($this->lAlcools, $nouveauAlcool);
+        function setLAlcools($nouveauAlcool){ 
+            array_push($this->lAlcools, $nouveauAlcool); //On ajoute a la liste d'alcools, l'alcool passée en paramètre
         }
-        
         function setLDiluants($nouveauDiluant){
-                array_push($this->lDiluants, $nouveauDiluant);
+            array_push($this->lDiluants, $nouveauDiluant);//On ajoute a la liste de diluants, le diluant passée en paramètre
         }
-
         function setLAutres($nouveauAutre){
-                array_push($this->lAutres, $nouveauAutre);
+            array_push($this->lAutres, $nouveauAutre);//On ajoute a la liste des autres boissons, la boisson passée en paramètre
         }
-
 
         /* -------------------------------- TO STRING ------------------------------- */
 
         function toString(){
-            $message="La liste d'alcools contient : ".$this->getLAlcools().", la liste de diluants contient : ".$this->getLDiluants().", la liste des autres boissons contient : ".$this->getLAutres();
-            return($message); //pour afficher le message il faudra faire un print_r
-        }
+            $message= "La liste d'alcools contient : ".$this->getLAlcools(). " ; la liste des diluants contient : ".$this->getLDiluants(). " ; la liste des autres boissons contient : ".$this->getLAutres(); //On récupére chaque liste de boissons et on les concatènes
+            return $message;
 
         /* -------------------------------------------------------------------------- */
         /*                            METHODES SPECIFIQUES                            */
         /* -------------------------------------------------------------------------- */
     }
+}
 
 ?>
