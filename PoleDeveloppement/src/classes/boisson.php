66d992a573b5cb631705b3688f3065f8fd48d8cd
Oier Cesat <oier.cesat@gmail.com>
2022-11-18 14:21:53 +0100

    Ajout de commentaires dans le fichier Boisson.php

--- PoleDeveloppement/src/classes/boisson.php
+++ PoleDeveloppement/src/classes/boisson.php
@@ -1,25 +1,43 @@
+/**
+     *@author @oiercesat <ocesat@iutbayonne.univ-pau.fr>
+     *@version ${1:1.0.0
+*/
+
 <?php
+
+
+    /**
+     * @brief Classe Boisson comportant un nom, un type, une quantité initiale et la quantité de boisson en cours
+     */
     class Boisson{
 
         /* -------------------------------------------------------------------------- */
         /*                                  ATTRIBUTS                                 */
         /* -------------------------------------------------------------------------- */
-        private $nomBoisson;
-        private $qtBoissonInitiale;
-        private $typeBoisson;
-        private $qtBoissonEnCours;
+        private $nomBoisson;            //nom de la boisson
+        private $typeBoisson;           //type de la boisson
+        private $qtBoissonInitiale;     //quantité initiale de la boisson
+        private $qtBoissonEnCours;      //quantité en cours/actuelle de la boisson
 
         /* -------------------------------------------------------------------------- */
         /*                                CONSTRUCTEUR                                */
         /* -------------------------------------------------------------------------- */
         
+
+        /**
+         * @brief constructeur d'une Boisson 
+         * @param string, int, int, int ou une boisson ou pas de paramètres 
+         * @return Boisson
+         */
         function __construct()
         {
 
-            $nbArguments= func_num_args();
-            $tabArguments=func_get_args();
+            $nbArguments= func_num_args();      //Nombre de parametres recu par le constructeur
+            $tabArguments=func_get_args();      //Stock les parametre dans un tableau
 
             switch ($nbArguments) {
+
+                // si le constructeur est par défaut
                 case 0:
                     $this->nomBoisson="";
                     $this->typeBoisson=0;
@@ -27,6 +45,7 @@
                     $this->qtBoissonEnCours=0;
                     break;
                 
+                // si le paramètre est une Boisson
                 case 1:
                     $this->nomBoisson=$tabArguments[0]->nomBoisson;
                     $this->typeBoisson=$tabArguments[0]->typeBoisson;
@@ -35,6 +54,7 @@
                     
                     break;
                 
+                // si les paramètres sont les attributs d'une boisson
                 case 4:
                     $this->nomBoisson=$tabArguments[0];
                     $this->typeBoisson=$tabArguments[1];
@@ -42,6 +62,8 @@
                     $this->qtBoissonEnCours=$tabArguments[3];
                     
                     break;
+
+                //si le nombre de paramètre n'est pas bon
                 default:
                     print("les parametres ne sont pas bon, les paramètres doivent etre soit nul, soit un objet de type Boisson, soit quatres parametres (nomBoisson, typeBoisson, qtBoissonInitiale, qtBoissonEnCours)");
                     break;
@@ -54,36 +76,80 @@
 
         /* --------------------------------- GETEUR --------------------------------- */
 
+        /**
+         * @brief retourne le nom de la boisson
+         * @return string 
+         */
         function getNomBoisson (){
             return ($this->nomBoisson);
-        }        
+        }      
+        
+        /**
+         * @brief retourne le type de la boisson
+         * @return int 
+         */
         function getTypeBoisson (){
             return ($this->typeBoisson);
         }        
+
+        /**
+         * @brief retourne la quantité initiale de la boisson
+         * @return int
+         */
         function getQtBoissonInitiale (){
             return ($this->qtBoissonInitiale);
-        }        
+        }      
+        
+        /**
+         * @brief retourne quantité en cours de la boisson
+         * @return int
+         */
         function getQtBoissonEnCours (){
             return ($this->qtBoissonEnCours);
         }        
 
         /* --------------------------------- SETEUR --------------------------------- */
 
+        /**
+         * @brief permet de modifier l'attribut nomBoisson
+         * @param string nouveauNomBoisson
+         */
+    
         function setNomBoisson($nouveauNomBoisson){
             $this->nomBoisson=$nouveauNomBoisson;
         }
+
+        /**
+         * @brief permet de modifier l'attribut typeBoisson
+         * @param int nouveauTypeBoisson
+         */
         function setTypeBoisson($nouveauTypeBoisson){
             $this->typeBoisson=$nouveauTypeBoisson;
         }
+
+        /**
+         * @brief permet de modifier l'attribut qtBoissonInitiale
+         * @param int nouvelleQtBoissonInitiale
+         */
         function setQtBoissonInitiale($nouvelleQtBoissonInitiale){
             $this->qtBoissonInitiale=$nouvelleQtBoissonInitiale;
         }
+
+        /**
+         * @brief permet de modifier l'attribut qtBoissonEnCours
+         * @param int nouvelleQtBoissonEnCours
+         */
         function setQtBoissonEnCours($nouvelleQtBoissonEnCours){
             $this->qtBoissonEnCours=$nouvelleQtBoissonEnCours;
         }
 
         /* -------------------------------- TO STRING ------------------------------- */
 
+
+        /**
+         * @brief retourne un message comportant les spécifiquations de l'objet Boisson 
+         * @return string message 
+         */
         function toString(){
             $message="Le nom de la boisson est : ".$this->getNomBoisson().", le type de la boisson est : ".$this->getTypeBoisson().", la quantité initiale est : ".$this->getQtBoissonInitiale().", la quantité de boisson en cours est : ".$this->getQtBoissonEnCours();
             return($message);
