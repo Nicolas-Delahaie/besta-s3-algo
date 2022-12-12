f0cd4b5c21b9b136338180972b431abd54dfa360
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2022-12-12 11:05:57 +0100

    Ajout commentaires Doxygen non complets

--- PoleDeveloppement/src/classes/Branche.php
+++ PoleDeveloppement/src/classes/Branche.php
@@ -1,19 +1,42 @@
 <?php
+    /** 
+     * @author @nicolasdelahaie <ndelahaie@iutbayonne.univ-pau.fr>
+     * @version ${1:1.0.0
+     */
+    
+    /**
+     * @brief Classe branche representant une possiblite de cocktail
+     */
     class Branche{
         /* -------------------------------------------------------------------------- */
         /*                                  ATTRIBUTS                                 */
         /* -------------------------------------------------------------------------- */
-        
-        private $pRecette, $qtBranche, $qtValeur;
+        /**
+         * @brief A REMPLIR (pRecette)
+         */
+        private $pRecette
+        /**
+         * @brief A REMPLIR (qtBranche)
+         */
+        private $qtBranche
+        /**
+         * @brief A REMPLIR (qtValeur)
+         */
+        private $qtValeur;
 
         /* -------------------------------------------------------------------------- */
         /*                                CONSTRUCTEUR                                */
         /* -------------------------------------------------------------------------- */
+        /**
+         * @brief Constructeur par defaut, par copie ou avec parametres
+         * @param attribut 1 de la classe : pRecette
+         * @param attribut 2 de la classe : qtBranche
+         * @param attribut 2 de la classe : qtValeur
+         */
         function __construct()
         {
             $nbArguments= func_num_args();
             $tabArguments=func_get_args();
-            
 
             switch ($nbArguments) {
                 case 0:
@@ -47,16 +70,41 @@
         /* -------------------------------------------------------------------------- */
 
         /* --------------------------------- GETTERS --------------------------------- */
+        /**
+         * @return l'attribut 1 de la classe : pRecette
+         */
         function getPRecette (){return ($this->pRecette);}
+        /**
+         * @return l'attribut 2 de la classe : qtBranche
+         */
         function getQtBranche (){return ($this->qtBranche);}  
+        /**
+         * @return l'attribut 3 de la classe : qtValeur
+         */
         function getQtValeur (){return ($this->qtValeur);}     
         
         /* --------------------------------- SETTERS --------------------------------- */
+        /**
+         * @brief set l'attribut 1 pRecette
+         * @param pRecette
+         */
         function setPRecette ($p){$this->pRecette = $p;}  
+        /**
+         * @brief set l'attribut 1 pRecette
+         * @param qtBranche
+         */
         function setQtBranche ($qtB){$this->qtBranche = $qtB;}  
+        /**
+         * @brief set l'attribut 1 pRecette
+         * @param qtValeur
+         */
         function setQtValeur ($qtV){$this->qtValeur = $qtV;} 
 
         /* -------------------------------- TO STRING ------------------------------- */
+        /**
+         * @brief Traduit la branche en une chaine de caracteres
+         * @return Chaine de caractere representant l objet
+         */
         function toString(){
             //Retourne la branche de la forme branche(volume utilise : X, valeur totale : X, suite de cocktails : X - X - Z)
             $message = "Branche (volume utilise : $this->qtBranche, valeur totale : $this->qtValeur, suite de cocktails : ";
