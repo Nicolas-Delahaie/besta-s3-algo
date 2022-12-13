70bee502fc884e6efcf66741416770325c4a71bf
Oier Cesat <oier.cesat@gmail.com>
2022-12-13 10:42:35 +0100

    verif dans le main.php

--- PoleDeveloppement/src/1dex.php
+++ PoleDeveloppement/src/1dex.php
@@ -3,7 +3,7 @@
 <body>
     <form action="main.php" method="POST">
         <p>Sélectionnez le nom de la recette </p>
-        <select name="nomBoisson">
+        <select name="nomBoisson" id="nomBoisson">
             <option value="default">Boisson</option>
             <?php
             $bdBoisson = fopen("bdBoisson.txt", "r");
@@ -11,7 +11,7 @@
                 $ligne = fgets($bdBoisson);
                 $ligneExplode = explode(",", $ligne);
                 $nomBoisson = $ligneExplode[0];
-                echo "<option value=''>".$nomBoisson."</option>";
+                echo "<option value='".$nomBoisson."'>".$nomBoisson."</option>";
             }
             fclose($bdBoisson);
             ?>
--- PoleDeveloppement/src/main.php
+++ PoleDeveloppement/src/main.php
@@ -1,55 +1,15 @@
-<html>
-
-<body>
-    <form action="main.php" method="POST">
-        <p>Sélectionnez le nom de la recette </p>
-        <select name="nomBoisson">
-            <option value="default">Boisson</option>
-            <?php
-            $bdBoisson = fopen("bdBoisson.txt", "r");
-            while (!(feof($bdBoisson))) {
-                $ligne = fgets($bdBoisson);
-                $ligneExplode = explode(",", $ligne);
-                $nomBoisson = $ligneExplode[0];
-                echo "<option value=''>".$nomBoisson."</option>";
-            }
-            fclose($bdBoisson);
-            ?>
-        </select>
-        <p>Sélectionnez la quantité de votre boisson</p>
-        <input type="number" name="qtBoisson" value="qtBoisson">
-        <br>
-        <input type="submit" value="Valider">
-    </form>
-</body>
-
-</html>
-
 
 <?php
-    include("../classes/Stock.php");
-    include("../classes/Boisson.php");
-    include("../classes/Recette.php");
+if ($_POST['qtBoisson']!="") {
 
-    $recettesExistantes = array();
-    $bdRecettes=fopen("../bdRecettes.txt", "r");
-    
-    while (! (feof($bdRecettes))) {
-        $ligne = fgets($bdRecettes);
-        $ligneExplode = explode(",", $ligne);
-        $uneRecetteBD = new Recette();
-        $uneRecetteBD->setNomRecette($ligneExplode[0]." ".$ligneExplode[1]);
-        $uneRecetteBD->setAlcool($ligneExplode[0]);
-        $uneRecetteBD->setDiluant($ligneExplode[1]);
-        
-        array_push($recettesExistantes, $uneRecetteBD);
+    if ($_POST['nomBoisson'] == 'default') {
+        header("Location: 1dex.php");
     }
+} 
 
-    $coca=new Boisson("coca",2,10,10);
-    $rhum=new Boisson("rhum",1,5,5);
-    
-    $stockSoiree=new Stock ();
-    $stockSoiree->setLDiluants($coca);
-    $stockSoiree->setLAlcools($rhum);
+else {
+    header("Location: 1dex.php");
+}
+var_dump($_POST);
 
 ?> 
