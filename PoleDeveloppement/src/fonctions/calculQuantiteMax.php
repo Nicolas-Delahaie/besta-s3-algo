59ffe8e59902f36aca719afa227f142c4b858e7e
unknown <alexandre.pascal.ep@gmail.com>
2022-12-13 18:33:52 +0100

    Ajout de la fonction calculQuantiteDesRecettes et ajout de commentaire doxygen dans calculQuantiteMax

--- PoleDeveloppement/src/fonctions/calculQuantiteMax.php
+++ PoleDeveloppement/src/fonctions/calculQuantiteMax.php
@@ -1,20 +1,31 @@
 <?php
-require_once('Stock.php');
 
-function calculQuantiteMax ($stockSoiree) {
+/**
+ * Summary of calculQuantiteMax
+ * @brief calcule la quantité totale d'alcool et de diluant que l'on a en stock (pas autre)
+ * @param Stock $stockSoiree contient toutes les listes de boissons
+ * @param int $tailleStockSoireeAlcool taille de la liste d'alcool du stock $stockSoiree
+ * @param int $tailleStockSoireeDiluant taille de la liste de diluants du stock $stockSoiree
+ * @return int $qtMax la quantité totale d'alcool et de diluant que l'on a en stock (pas autre)
+ */
+function calculQuantiteMax ($stockSoiree,$tailleStockSoireeAlcool,$tailleStockSoireeDiluant) {
     $qtMax = 0;
 
-    foreach ($stockSoiree->getLAlcools() as $value) { //Pour chaque élément de la liste d'alcools de notre stock
-        $quantite = $value->getQtBoissonInitiale(); //on récupère sa quantité initiale 
-        $qtMax = $qtMax + $quantite; //on l'ajoute à la variable qtMax 
+    for ($i=0; $i < $tailleStockSoireeAlcool; $i++) { 
+        $alcool = $stockSoiree->getLAlcools()[$i]; //Pour chaque élément de la liste d'alcools de notre stock
+        $quantite = $alcool->getQtBoissonInitiale();//on récupère sa quantité initiale
+        $qtMax = $qtMax + $quantite;//on l'ajoute à la variable qtMax 
+
     }
 
-    foreach ($stockSoiree->getLDiluants() as $value) { //Pour chaque élément de la liste de diluants de notre stock
-        $quantite = $value->getQtBoissonInitiale(); //on récupère sa quantité initiale 
+    for ($i=0; $i < $tailleStockSoireeDiluant; $i++) { 
+        $diluant = $stockSoiree->getLDiluants()[$i];  //Pour chaque élément de la liste de diluants de notre stock
+        $quantite = $diluant->getQtBoissonInitiale(); //on récupère sa quantité initiale 
         $qtMax = $qtMax + $quantite; //on l'ajoute à la variable qtMax
     }
 
     return $qtMax;
-   }
-   
+
+}
+
 ?>
